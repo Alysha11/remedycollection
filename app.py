5330c24d647ed430db019dc3dda01454e729e851
Alesha <alyshanee@gmail.com>
2020-01-03 12:56:51 +0000

    connect to mongodb, set environment variables,  create templates folder

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+  import env 
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'remedy_collection'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+
+mongo = PyMongo(app)
+
 
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_remedies')
+def get_remedies():
+    return render_template("remedies.html", remedies=mongo.db.remedies.find())
 
 
 if __name__ == '__main__':
